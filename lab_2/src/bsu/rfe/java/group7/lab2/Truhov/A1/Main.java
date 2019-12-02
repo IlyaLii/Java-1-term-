fc8198a28992652e4f6a3233a9d43b4c983b6a1d
Ilya <agronat88@icloud.com>
2019-12-02 23:19:57 +0300

    lab_2

---
+++ lab_2/src/bsu/rfe/java/group7/lab2/Truhov/A1/Main.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group7.lab2.Truhov.A1;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Formula frame = new Formula();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
